fa970d9ce5efa9807eeac5fe32057c8e935d58ca
alex94025 <78575199+alex94025@users.noreply.github.com>
2022-06-09 10:33:07 +0800

    Written code for fetching dispense log

--- Pet Feeder Machine Problem/DispenseLogActivity.cs
+++ Pet Feeder Machine Problem/DispenseLogActivity.cs
@@ -22,7 +22,8 @@ namespace Pet_Feeder_Machine_Problem
     public class DispenseLogActivity : Activity
     {
         RecyclerView recyclerView;
-        List<LogRecord> logRecords;
+        //List<LogRecord> logRecords;
+        HttpClient client;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -32,17 +33,40 @@ namespace Pet_Feeder_Machine_Problem
             
             recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView1);
 
-            logRecords = new List<LogRecord>();
+    /*      logRecords = new List<LogRecord>();
             logRecords.Add(new LogRecord { time = "22:51:12", temperature = "33.0 C", humidity = "60%", serving = "Large", mode = "Manual" });
             logRecords.Add(new LogRecord { time = "23:51:12", temperature = "22.0 C", humidity = "63%", serving = "Medium", mode = "Manual" });
             logRecords.Add(new LogRecord { time = "00:51:12", temperature = "25.0 C", humidity = "56%", serving = "Large", mode = "Auto" });
             logRecords.Add(new LogRecord { time = "09:51:12", temperature = "26.0 C", humidity = "58%", serving = "Large", mode = "Manual" });
             logRecords.Add(new LogRecord { time = "07:51:12", temperature = "24.0 C", humidity = "61%", serving = "Small", mode = "Auto" });
+    */
+            RunOnUiThread(async () =>
+            {
+                await UpdateLog();
+            });
+        }
 
-            recyclerView.SetLayoutManager(new Android.Support.V7.Widget.LinearLayoutManager(recyclerView.Context));
-            RecyclerViewAdapter adapter = new RecyclerViewAdapter(logRecords);
-            recyclerView.SetAdapter(adapter);
+        public async Task UpdateLog()
+        {
+            client = new HttpClient();
 
+            string url = RESTAPI.url() + $"getDispenseLogs.php";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var responseObject = JsonConvert.DeserializeObject<List<LogRecord>>(result);
+
+                recyclerView.SetLayoutManager(new Android.Support.V7.Widget.LinearLayoutManager(recyclerView.Context));
+                RecyclerViewAdapter adapter = new RecyclerViewAdapter(responseObject);
+                recyclerView.SetAdapter(adapter);
+            }
+            else
+            {
+                Toast.MakeText(this, "Error in fetching log records", ToastLength.Long).Show();
+            }
         }
     }
 }
